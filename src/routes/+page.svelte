3f6e3cbccbd37ae0e8f37338401527f7156fb4e6
Mohammed Saleh <mohammed_500@outlook.dk>
2025-08-30 20:47:07 +0200

    new build

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script context="module" lang="ts">
+  import type { Load } from '@sveltejs/kit';
+
+  export const load: Load = async () => {
+    return {
+      status: 302,
+      redirect: '/twitch-widget'
+    };
+  };
+</script>
